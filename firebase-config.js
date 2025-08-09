8a579785e5447cbe3432b6a0aee256152137f658
india8155 <120180496+india8155@users.noreply.github.com>
2025-08-09 17:04:01 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBFM1T3GctmY9-gX1ijPxmDW7CqgOVu8cc",
-  authDomain: "keval-photography.firebaseapp.com",
-  projectId: "keval-photography",
-  storageBucket: "keval-photography.firebasestorage.app",
-  messagingSenderId: "512590520552",
-  appId: "1:512590520552:web:f4cb1fce144b62e96da95d",
-  measurementId: "G-6E8KDW8W99" // optional
-};
