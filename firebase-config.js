3193e4a93a4f4734adbc25471681431d5bb41bed
india8155 <120180496+india8155@users.noreply.github.com>
2025-08-09 15:19:46 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+// Replace the values with your Firebase web app config (from Firebase console)
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFM1T3GctmY9-gX1ijPxmDW7CqgOVu8cc",
+  authDomain: "keval-photography.firebaseapp.com",
+  projectId: "keval-photography",
+  storageBucket: "keval-photography.firebasestorage.app",
+  messagingSenderId: "512590520552",
+  appId: "1:512590520552:web:f4cb1fce144b62e96da95d"
+};
