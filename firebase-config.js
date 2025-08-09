98d05bee549602053365e39bdb43a8677b7f3645
india8155 <120180496+india8155@users.noreply.github.com>
2025-08-09 16:57:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// ../firebase-config.js
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyBFM1T3GctmY9-gX1ijPxmDW7CqgOVu8cc",
   authDomain: "keval-photography.firebaseapp.com",
@@ -7,5 +5,5 @@ export const firebaseConfig = {
   storageBucket: "keval-photography.firebasestorage.app",
   messagingSenderId: "512590520552",
   appId: "1:512590520552:web:f4cb1fce144b62e96da95d",
-  measurementId: "G-6E8KDW8W99"
+  measurementId: "G-6E8KDW8W99" // optional
 };
